c00783119c59a3989a1a8ec56ff999a7a5cd05a9
Nadir Hicham <hnadir.dev@gmail.com>
2023-07-17 22:25:12 +0100

    Stream audio 0.4

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import os
 import pafy
 from flask import Flask
 from asgiref.wsgi import WsgiToAsgi
 
 
-project_root = os.path.dirname(__file__)
-template_path = os.path.join(project_root, "app/templates")
-app = Flask(__name__, template_folder=template_path)
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -14,7 +11,7 @@ def index():
     url = "https://www.youtube.com/watch?v=6U1wyOyuzj4"
     video = pafy.new(url)
 
-    return video.getbestaudio().url
+    return "video.getbestaudio().url"
 
 
 asgi_app = WsgiToAsgi(app)
