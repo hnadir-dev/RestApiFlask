1d5ff0b1b0694952318a172b4b103ae7d52b3de6
Nadir Hicham <hnadir.dev@gmail.com>
2023-07-17 22:33:36 +0100

    Stream audio 0.5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pafy
-from flask import Flask
+from flask import Flask,render_template
 from asgiref.wsgi import WsgiToAsgi
 
 
@@ -11,7 +11,8 @@ def index():
     url = "https://www.youtube.com/watch?v=6U1wyOyuzj4"
     video = pafy.new(url)
 
-    return "video.getbestaudio().url"
+    return render_template("index.html", url = video.getbestaudio().url)
+    #return "video.getbestaudio().url"
 
 
 asgi_app = WsgiToAsgi(app)
